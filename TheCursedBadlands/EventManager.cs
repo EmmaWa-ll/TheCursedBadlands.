0dfb126e0e330e87e685a60c3174dddbf609c903
Emma Wall <emma.wall98@gmail.com>
2025-09-19 08:16:00 +0200

    Changed eventmanager

--- TheCursedBadlands/EventManager.cs
+++ TheCursedBadlands/EventManager.cs
@@ -22,7 +22,7 @@
         public static void BanditAmbush(Player p)
         {
 
-            Console.WriteLine($"\n Bandits jump from behind the rock and open fire!  You manage to escape but not withouta few wounds. (-10 HP)");
+            Console.WriteLine($"\n≫ Bandits jump from behind the rock and open fire!  You manage to escape but not withouta few wounds. (-10 HP) ≪");
             p.CurrentHP -= 10;
             GameHelper.Pause();
         }
@@ -32,13 +32,13 @@
 
             int gold = randomEvent.Next(5, 16);
             p.Gold += gold;
-            Console.WriteLine($"\nWhile digging through and abandoned camp you stuble upon a hidden pouch of coins. (+{gold} gold)");
+            Console.WriteLine($"\n≫ While digging through and abandoned camp you stuble upon a hidden pouch of coins. (+{gold} gold) ≪");
             GameHelper.Pause();
         }
 
         public static void SnakeBite(Player p)
         {
-            Console.WriteLine($"\nYou step too close to a cactus and a rattlesnake lunges ou! It's fangs pierce your boot. (-5 HP)");
+            Console.WriteLine($"\n≫ You step too close to a cactus and a rattlesnake lunges ou! It's fangs pierce your boot. (-5 HP) ≪");
             p.CurrentHP = Math.Max(0, p.CurrentHP - 5);
             GameHelper.Pause();
         }
